088b57ea3593f416fbbf431878ce30cef12a81f6
Cole Caccamise <colecaccamise@gmail.com>
2024-12-17 12:41:02 -0500

    chore: add stripe structs

---
+++ backend/models/stripe.go
@@ -0,0 +1,16 @@
+package models
+
+type SubscriptionPlan struct {
+	Name           string `json:"name"`
+	ProductID      string `json:"product_id"`
+	PriceID        string `json:"price_id"`
+	PriceLookupKey string `json:"price_lookup_key"`
+	ProductActive  bool   `json:"product_active"`
+	PriceActive    bool   `json:"price_active"`
+	Interval       string `json:"interval"`
+	Price          int    `json:"price"`
+}
+
+type CreateCheckoutSessionRequest struct {
+	PriceLookupKey string `json:"price_lookup_key"`
+}
