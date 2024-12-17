df6802245395764c6ac9b59fe3081b1625f9048c
Cole Caccamise <colecaccamise@gmail.com>
2024-12-17 12:41:20 -0500

    chore: add stripe customer id to uesr

--- backend/models/user.go
+++ backend/models/user.go
@@ -41,23 +41,24 @@ type ChangePasswordRequest struct {
 }
 
 type User struct {
-	ID                      uuid.UUID  `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
-	FirstName               string     `gorm:"" json:"first_name"`
-	LastName                string     `gorm:"" json:"last_name"`
-	Email                   string     `gorm:"unique;not null" json:"email"`
-	UpdatedEmail            string     `gorm:"" json:"updated_email"`
-	CreatedAt               time.Time  `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt               time.Time  `gorm:"autoUpdateTime" json:"updated_at"`
-	HashedPassword          string     `gorm:"" json:"hashed_password"`
-	UpdatedEmailAt          *time.Time `gorm:"default:null" json:"updated_email_at"`
-	UpdatedEmailConfirmedAt *time.Time `gorm:"default:null" json:"updated_email_confirmed_at"`
-	EmailConfirmedAt        *time.Time `gorm:"default:null" json:"email_confirmed_at"`
-	IsAdmin                 bool       `gorm:"default:false" json:"is_admin"`
-	AvatarUrl               string     `gorm:"default:null" json:"avatar_url"`
-	AvatarThumbnailUrl      string     `gorm:"default:null" json:"avatar_thumbnail_url"`
-	DeletedAt               *time.Time `gorm:"default:null" json:"deleted_at"`
-	RestoredAt              *time.Time `gorm:"default:null" json:"restored_at"`
+	ID                       uuid.UUID  `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
+	FirstName                string     `gorm:"" json:"first_name"`
+	LastName                 string     `gorm:"" json:"last_name"`
+	Email                    string     `gorm:"unique;not null" json:"email"`
+	UpdatedEmail             string     `gorm:"" json:"updated_email"`
+	CreatedAt                time.Time  `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt                time.Time  `gorm:"autoUpdateTime" json:"updated_at"`
+	HashedPassword           string     `gorm:"" json:"hashed_password"`
+	UpdatedEmailAt           *time.Time `gorm:"default:null" json:"updated_email_at"`
+	UpdatedEmailConfirmedAt  *time.Time `gorm:"default:null" json:"updated_email_confirmed_at"`
+	EmailConfirmedAt         *time.Time `gorm:"default:null" json:"email_confirmed_at"`
+	IsAdmin                  bool       `gorm:"default:false" json:"is_admin"`
+	AvatarUrl                string     `gorm:"default:null" json:"avatar_url"`
+	AvatarThumbnailUrl       string     `gorm:"default:null" json:"avatar_thumbnail_url"`
+	DeletedAt                *time.Time `gorm:"default:null" json:"deleted_at"`
+	RestoredAt               *time.Time `gorm:"default:null" json:"restored_at"`
 	SecurityVersionChangedAt *time.Time `gorm:"default:null" json:"security_version_changed_at"`
+	CustomerID               string     `json:"customer_id"`
 }
 
 type UserIdentityResponse struct {
