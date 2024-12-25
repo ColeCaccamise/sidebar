4c5accd17fe3c2bbb04de0f4b889fed51ef7ef47
Cole Caccamise <colecaccamise@gmail.com>
2024-12-25 06:55:51 -0500

    feat: add disabled and destructive state to dropdowns

--- frontend/components/ui/dropdown-menu-item.tsx
+++ frontend/components/ui/dropdown-menu-item.tsx
@@ -1,44 +1,50 @@
 import Link from 'next/link';
 
 export default function DropdownMenuItem({
-	icon,
-	label,
-	href,
-	kbd,
-	handleClick,
+  icon,
+  label,
+  href,
+  kbd,
+  handleClick,
+  disabled,
+  destructive,
 }: {
-	icon: undefined | null | React.ReactNode;
-	label: string;
-	href?: string;
-	kbd?: string;
-	handleClick?: () => void;
+  icon?: undefined | null | React.ReactNode;
+  label?: string;
+  href?: string;
+  kbd?: string;
+  handleClick?: () => void;
+  disabled?: boolean;
+  destructive?: boolean;
 }) {
-	const iconStyles =
-		'ml-auto hidden font-sans text-xs text-typography-weak group-hover:text-typography-strong';
-	const buttonStyles =
-		'flex items-center gap-3 cursor-pointer hover:bg-hover w-full p-2 rounded-md data-[focus]:bg-fill no-underline text-sm text-typography-weak hover:text-typography-strong';
+  const iconStyles = 'ml-auto font-sans text-xs text-typography-weak ';
+  const buttonStyles = `flex items-center gap-3 w-full p-2 rounded-md data-[focus]:bg-fill no-underline text-sm ${
+    disabled
+      ? 'cursor-not-allowed opacity-50'
+      : destructive
+        ? 'cursor-pointer hover:bg-hover text-error hover:text-error'
+        : 'cursor-pointer hover:bg-hover text-typography-weak hover:text-typography-strong'
+  }`;
 
-	if (href) {
-		return (
-			<Link
-				href={href}
-				className={buttonStyles}
-			>
-				{icon}
-				<span className='text-inherit'>{label}</span>
-				{kbd && <kbd className={iconStyles}>{kbd}</kbd>}
-			</Link>
-		);
-	} else {
-		return (
-			<button
-				className={buttonStyles}
-				onClick={handleClick}
-			>
-				{icon}
-				<span className='text-inherit'>{label}</span>
-				{kbd && <kbd className={iconStyles}>{kbd}</kbd>}
-			</button>
-		);
-	}
+  if (href && !disabled) {
+    return (
+      <Link href={href} className={buttonStyles}>
+        {icon}
+        <span className="text-inherit">{label}</span>
+        {kbd && <kbd className={iconStyles}>{kbd}</kbd>}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        className={buttonStyles}
+        onClick={disabled ? undefined : handleClick}
+        disabled={disabled}
+      >
+        {icon}
+        <span className="text-inherit">{label}</span>
+        {kbd && <kbd className={iconStyles}>{kbd}</kbd>}
+      </button>
+    );
+  }
 }
