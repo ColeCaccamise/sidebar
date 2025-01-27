85f77d3fb964ddb4c7d64452601dd9e80a8ed922
Cole Caccamise <colecaccamise@gmail.com>
2025-01-27 08:02:30 -0500

    feat: add custom ai sdk

---
+++ backend/api/ai.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/colecaccamise/go-backend/util"
+)
+
+func (s *Server) handleChat(w http.ResponseWriter, r *http.Request) error {
+	chatReq := new(util.ChatRequest)
+
+	if err := util.DecodeBody(r.Body, chatReq); err != nil {
+		return WriteJSON(w, http.StatusBadRequest, Error{Error: err.Error(), Code: "invalid_request"})
+	}
+
+	aiClient, err := util.NewAIClient(&util.NewAIClientOpts{
+		ModelType: util.DeepSeek,
+		Model:     util.DeepseekChat,
+		APIKey:    os.Getenv("DEEPSEEK_API_KEY"),
+	})
+	if err != nil {
+		return WriteJSON(w, http.StatusInternalServerError, Error{Error: err.Error(), Code: "internal_server_error"})
+	}
+
+	response, err := aiClient.GenerateText(&util.GenerateTextOpts{
+		Prompt: chatReq.Prompt,
+		Stream: true,
+	})
+
+	if err != nil {
+		return WriteJSON(w, http.StatusInternalServerError, Error{Error: err.Error(), Code: "internal_server_error"})
+	}
+
+	return WriteJSON(w, http.StatusOK, response)
+}
