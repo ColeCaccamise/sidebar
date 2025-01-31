db27e29615d4529dd4d287c9f4490a59680b1849
Cole Caccamise <colecaccamise@gmail.com>
2025-01-30 19:01:21 -0500

    chore: add redis helper

---
+++ backend/util/redis.go
@@ -0,0 +1,65 @@
+package util
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"os"
+)
+
+type RedisClient struct {
+	Client *redis.Client
+}
+
+func NewRedisClient() *RedisClient {
+	redisUrl := fmt.Sprintf("rediss://default:%s@%s:%s", os.Getenv("REDIS_PASSWORD"), os.Getenv("REDIS_ENDPOINT"), os.Getenv("REDIS_PORT"))
+
+	fmt.Println("redisUrl:", redisUrl)
+
+	opt, _ := redis.ParseURL(redisUrl)
+	client := redis.NewClient(opt)
+
+	return &RedisClient{
+		Client: client,
+	}
+}
+
+type RedisSetOpts struct {
+	Key   string
+	Value string
+}
+
+func (r *RedisClient) Set(ctx context.Context, opts RedisSetOpts) error {
+	return r.Client.Set(ctx, opts.Key, opts.Value, 0).Err()
+}
+
+type RedisGetOpts struct {
+	Key string
+}
+
+func (r *RedisClient) Get(ctx context.Context, opts RedisGetOpts) (interface{}, error) {
+	val, err := r.Client.Get(ctx, opts.Key).Result()
+	if err != nil {
+		return "", err
+	}
+
+	return val, nil
+}
+
+func (r *RedisClient) GetJSON(ctx context.Context, opts RedisGetOpts) (interface{}, error) {
+	val, err := r.Client.Get(ctx, opts.Key).Result()
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println(val)
+
+	var data interface{}
+	err = json.Unmarshal([]byte(val), &data)
+	if err != nil {
+		return "", err
+	}
+
+	return data, nil
+}
