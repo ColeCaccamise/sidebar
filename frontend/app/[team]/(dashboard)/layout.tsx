d3bd12bb623c7f668c44949aefb8422539960a03
Cole Caccamise <colecaccamise@gmail.com>
2025-02-01 06:16:19 -0500

    chore: add team not found page

--- frontend/app/[team]/(dashboard)/layout.tsx
+++ frontend/app/[team]/(dashboard)/layout.tsx
@@ -1,15 +1,14 @@
 'use client';
 
 import TeamContainer from '@/app/[team]/(dashboard)/team-container';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import api from '@/lib/axios';
 import {
   useQuery,
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
-import { Team } from '@/types';
+import { notFound, useRouter } from 'next/navigation';
 
 export default function Layout({
   children,
@@ -21,24 +20,26 @@ export default function Layout({
   const router = useRouter();
   const queryClient = new QueryClient();
   function TeamContent() {
-    // const { data: team } = useQuery({
-    //   queryKey: ['team'],
-    //   queryFn: async () => {
-    //     const response = await api.get(`/teams/${params.team}`, {
-    //       withCredentials: true,
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //     });
-    //     return response.data.data.team;
-    //   },
-    // });
+    const { data: team, isLoading } = useQuery({
+      queryKey: ['team'],
+      queryFn: async () => {
+        const response = await api
+          .get(`/teams/${params.team}`, {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+          .then((res) => res.data.data.team)
+          .catch(() => null);
+        console.log('response', response);
+        return response;
+      },
+    });
 
-    const team: Team = {
-      id: '1',
-      slug: 'caccamise-1',
-      name: 'Caccamise',
-    };
+    if (!isLoading && team === null) {
+      return notFound();
+    }
 
     return (
       <TeamContainer slug={params.team} team={team}>
